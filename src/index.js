3fdb85999eaeb3e6339bc76a6549c5de17f71985
Jouko Karvonen <joukokar@gmail.com>
2017-07-29 12:47:51 +0900

    Try to get firebase working with facebook auth

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ import firebase from 'firebase';
 import firebaseConfig from './config/firebase';
 import registerServiceWorker from './registerServiceWorker';
 
-var provider = new firebase.auth.FacebookAuthProvider();
+firebase.initializeApp(firebaseConfig);
+const provider = new firebase.auth.FacebookAuthProvider();
+provider.setCustomParameters({
+  'display': 'popup'
+});
+
 firebase.auth().signInWithPopup(provider).then(function(result) {
   // This gives you a Facebook Access Token. You can use it to access the Facebook API.
   var token = result.credential.accessToken;
@@ -16,6 +21,7 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
   console.log(result)
   // ...
 }).catch(function(error) {
+  console.error('error', error)
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
@@ -26,7 +32,6 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
   // ...
 });
 
-firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
