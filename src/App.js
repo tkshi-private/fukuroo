dfcb1f0f3a66f9e066ca8dd53853e6373b805762
Takashi Endo <endotakashi1992@gmail.com>
2017-07-29 11:14:31 +0900

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hataraco!!</h1>
+        <img src="./mig.jpeg" ></img>
+      </div>
+    );
+  }
+}
+
+export default App;
