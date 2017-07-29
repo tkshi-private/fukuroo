b03bf10099c5e57faf69b5d5b8f8724f869c0d70
Jouko Karvonen <joukokar@gmail.com>
2017-07-29 18:27:18 +0900

    Real data sync!!

--- src/store/projects.js
+++ src/store/projects.js
@@ -1,8 +1,8 @@
 import {action, autorun, observable} from "mobx";
-import firebaseConfig from '../config/firebase'
-import firebase from 'firebase'
-import User from './user'
 
+import User from './user'
+import firebase from 'firebase'
+import firebaseConfig from '../config/firebase'
 
 class Project {
     @observable image_url = "";
@@ -17,49 +17,6 @@ class Projects {
     @observable projects = [];
 }
 
-let projects = new Projects()
-
-let project = new Project()
-project.pid = 1
-project.image_url = "https://static.pepy.jp/wp-content/uploads/2015/01/06115944/540bca3b8a7377bf8aabd4c1e751a31c-e1483577178335.jpg";
-project.title = "ハタラコ！！";
-project.abstract = "働け！";
-project.valuation = 100;
-
-projects.projects.push(
-  project
-)
-
-const anotherProject = new Project()
-anotherProject.pid = 2
-anotherProject.image_url = 'http://kyuryou-soshiki.kanagawa-u.ac.jp/aiti/wp-content/uploads/sites/43/2015/10/sample-icon.png';
-anotherProject.title = 'みんなで起業！';
-anotherProject.abstract = '日本にイノベーションを起こすために、新しい事業を創発しましょう。';
-anotherProject.valuation = 1000;
-
-projects.projects.push(anotherProject)
-
-
-
-const newProject = new Project()
-newProject.pid = 3
-newProject.image_url = "https://static.pepy.jp/wp-content/uploads/2015/01/06115944/540bca3b8a7377bf8aabd4c1e751a31c-e1483577178335.jpg";
-newProject.title = "ハタラコ！！";
-newProject.abstract = "働け！";
-newProject.valuation = 100;
-
-const user = new User()
-user.name = "林田敦"
-
-let member = {
-  role : "エンジニア",
-  stock_amount : 100,
-  stock_share : 50,
-  user : user
-}
-
-newProject.members.push(member)
-
-projects.projects.push(newProject)
+const projects = new Projects()
 
 export default projects.projects
--- src/syncFirebase.js
+++ src/syncFirebase.js
@@ -1,5 +1,6 @@
-import firebase from 'firebase'
+import _ from 'lodash';
 import config from './config/firebase'
+import firebase from 'firebase'
 import projects from './store/projects'
 firebase.initializeApp(config);
 
@@ -7,8 +8,40 @@ firebase.initializeApp(config);
 var database = firebase.database();
 
 export function startSyncFirebaseData(){
-  var starCountRef = firebase.database().ref('projects/');
-  starCountRef.on('value', function(snapshot) {
-    projects.push(snapshot.val()['-KqCBf092NKmFktgywPp'])
+  // var starCountRef = firebase.database().ref('projects/');
+  // starCountRef.on('value', function(snapshot) {
+  //   projects.push(snapshot.val()['-KqCBf092NKmFktgywPp'])
+  // });
+
+  // sync projects
+  const projectsRef = firebase.database().ref('projects');
+
+  // projectsRef.on('value', (data) => {
+  //   _.map(data.val(), (value, key) => {
+  //     const newProject = value;
+  //     newProject.pid = key;
+  //     projects.push(newProject)
+  //   })
+  // })
+
+  projectsRef.on('child_added', (data) => {
+    console.log('added', data.val())
+    const newProject = data.val();
+    newProject.pid = data.key;
+    projects.push(newProject);
+  });
+
+  projectsRef.on('child_changed', (data) => {
+    console.log('changed', data.val())
+    const newProject = data.val();
+    newProject.pid = data.key;
+    const index = _.findIndex(projects, p => p.pid === data.key)
+    projects[index] = newProject;
+  });
+
+  projectsRef.on('child_removed', (data) => {
+    console.log('removed', data.val())
+    const index = _.findIndex(projects, p => p.pid === data.key)
+    projects.splice(index, 1)
   });
 }
