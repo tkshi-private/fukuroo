e94fa5db6516d76ea08275e9d686c1f525fc7c8a
Jouko Karvonen <joukokar@gmail.com>
2017-07-29 12:09:38 +0900

    Add firebase connection

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React, { Component } from 'react';
+
+import firebase from 'firebase';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hataraco!!</h1>
-        <img src="./mig.jpeg" ></img>
+        <img src="./mig.jpeg" alt="fukuro"></img>
       </div>
     );
+  }
+
+  componentWillMount() {
+    var ref = firebase.database().ref("users")
+    var userId = firebase.auth().currentUser;
+    ref.on('value', (snapshot) => {
+      console.log(snapshot.val());
+    })
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
+import './index.css';
+
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import firebase from 'firebase';
+import firebaseConfig from './config/firebase';
 import registerServiceWorker from './registerServiceWorker';
+
+var provider = new firebase.auth.FacebookAuthProvider();
+firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  console.log(result)
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
