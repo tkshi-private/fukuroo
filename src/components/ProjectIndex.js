e0f4ebea43fc57156243e9ec9a37de7d2a87359d
y-okada <y-okada@y-okada-no-MacBook-Air.local>
2017-07-29 17:01:31 +0900

    modify list-page

--- src/components/ProjectIndex.js
+++ src/components/ProjectIndex.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import './ProjectIndex.css';
 
 class ProjectIndex extends Component {
   render() {
+    let image_url = 'http://kyuryou-soshiki.kanagawa-u.ac.jp/aiti/wp-content/uploads/sites/43/2015/10/sample-icon.png';
+    let title = 'みんなで起業！';
+    let abstract = '日本にイノベーションを起こすために、新しい事業を創発しましょう。';
+    let valuation = '¥10,000,000';
+
+    let list = [];
+    let data = [
+       {  }
+      ,{  }
+      ,{  }
+    ];
+
+    for(let i in data){
+      list.push(
+        <div className="list-item">
+          <div className="image-area">
+            <img src={image_url} alt="icon" />
+          </div>
+          <div className="row">
+            <div>{valuation}</div>
+            <div>{title}</div>
+            <div>{abstract}</div>
+          </div>
+          <div className="valuation-area">
+            <div>button</div>
+          </div>
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>プロジェクト一覧画面</h1>
+        <div className="list-container">
+          {list}
+        </div>
       </div>
     );
   }
