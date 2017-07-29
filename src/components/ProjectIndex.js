df8ee4f7d8d7a3ef35a3a8ea171fc3795313efb3
Jouko Karvonen <joukokar@gmail.com>
2017-07-29 15:08:07 +0900

    New project

--- src/components/ProjectIndex.js
+++ src/components/ProjectIndex.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+import { Link } from 'react-router-dom'
+
 class ProjectIndex extends Component {
   render() {
     return (
       <div className="App">
         <h1>プロジェクト一覧画面</h1>
+        <button>
+          <Link to="/new-project">新規プロジェクト追加</Link>
+        </button>
       </div>
     );
   }
---
+++ src/components/ProjectNew.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import { Link } from 'react-router-dom'
+
+class ProjectNew extends Component {
+  render() {
+    return (
+      <div className="ProjectNew">
+        <h1>新規プロジェクト登録</h1>
+
+        <form>
+          <div className="form-group">
+            <label>プロジェクト名</label>
+            <input className="form-control" type="text"/>
+          </div>
+
+          <div className="form-group">
+            <label>イメージ</label>
+            <input className="form-control" type="text"/>
+          </div>
+
+          <div className="form-group">
+            <label>詳細</label>
+            <textarea className="form-control"></textarea>
+          </div>
+
+          <button className="btn btn-primary btn-block col-sm-12">
+            登録
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ProjectNew;
