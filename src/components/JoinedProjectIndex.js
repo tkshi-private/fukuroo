53cfb42c6a587445617dc72569bff31ce1c474f1
Takashi Endo <endotakashi1992@gmail.com>
2017-07-29 18:24:02 +0900

    joined project page

---
+++ src/components/JoinedProjectIndex.js
@@ -0,0 +1,48 @@
+import './ProjectIndex.css';
+
+import React, { Component } from 'react';
+
+import { Link } from 'react-router-dom'
+import _ from 'lodash';
+import { observer } from "mobx-react";
+import projects from '../store/projects';
+
+@observer
+class ProjectIndex extends Component {
+  render() {
+    const list = _.map(projects, (project) => {
+      return (
+        <div className="list-item" key={project.pid}>
+          <div className="image-area">
+            <img src={project.image_url} alt="icon" />
+          </div>
+          <div className="row">
+            <div>{project.valuation}</div>
+            <div>{project.title}</div>
+            <div>{project.abstract}</div>
+          </div>
+          <div className="valuation-area">
+            <button className="btn btn-default">
+              参加する
+            </button>
+          </div>
+        </div>
+      );
+    })
+
+    return (
+      <div className="App">
+        <h1>参加中のプロジェクト一覧画面</h1>
+        <button className="btn btn-default">
+          <Link to="/new-project">新規プロジェクト追加</Link>
+        </button>
+
+        <div className="list-container">
+          {list}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ProjectIndex;
