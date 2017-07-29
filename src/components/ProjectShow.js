6989fb7a8d5aad12f3b5dee508a0f7e0d68ddfa8
Atsushi Hayashida <a.hayashida@n-village.co.jp>
2017-07-29 17:49:48 +0900

    before marge

--- src/components/ProjectShow.js
+++ src/components/ProjectShow.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-
 import {observer} from "mobx-react";
-import projects from '../store/project'
+import projects from '../store/projects'
 
 @observer
 class ProjectShow extends Component {
--- src/store/project.js
+++ src/store/project.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import User from './user'
-import {observable, autorun, action} from "mobx";
-=======
 import {action, autorun, observable} from "mobx";
->>>>>>> b06776ac3e619b424799ce12e973c1bd00ef2186
 
 export default class Project {
     @observable image_url = "";
@@ -12,26 +7,3 @@ export default class Project {
     @observable valuation = "";
     @observable members = [];
 }
-<<<<<<< HEAD
-
-let project = new Project()
-project.image_url = "https://static.pepy.jp/wp-content/uploads/2015/01/06115944/540bca3b8a7377bf8aabd4c1e751a31c-e1483577178335.jpg";
-project.title = "ハタラコ！！";
-project.abstract = "働け！";
-project.valuation = 100;
-
-let user = new User()
-user.name = "林田敦"
-
-let member = {
-  role : "エンジニア",
-  stock_amount : 100,
-  stock_share : 50,
-  user : user
-}
-
-project.members.push(
-  member
-)
-=======
->>>>>>> b06776ac3e619b424799ce12e973c1bd00ef2186
