21a9846e635fbe46127a1755f02c9d93d88c1e4b
Jouko Karvonen <joukokar@gmail.com>
2017-07-29 14:37:46 +0900

    Separate navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+
+import { Link } from 'react-router-dom'
+import firebase from 'firebase';
+
+const provider = new firebase.auth.FacebookAuthProvider();
+provider.setCustomParameters({
+  'display': 'iframe'
+});
+
+class NavBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  render() {
+    const currentUser = this.state.currentUser ?
+      (<span>ユーザー {this.state.currentUser.email}</span>) :
+      (<button onClick={this.login}>SignIn</button>);
+    return (
+      <div className="row">
+        <div className="col-sm-12">
+          
+          <div className="pull-right">
+            {currentUser}
+          </div>
+
+          <ul className="list-inline">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/projects">プロジェクト一覧</Link></li>
+            <li><Link to="/projects/1">プロジェクト詳細</Link></li>
+          </ul>
+
+          <hr/>
+        </div>
+      </div>
+    )
+  }
+
+  login() {
+    firebase.auth().signInWithPopup(provider).then((result) => {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+
+      this.setState({
+        currentUser: {
+          displayName: user.displayName,
+          email: user.email,
+        }
+      })
+      console.log('Signed in as', token, user)
+
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+
+      console.error('error', errorCode, errorMessage, email, credential)
+    });
+  }
+
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+
+        // User is signed in.
+        console.log('aaa', user)
+
+        this.setState({
+          currentUser: {
+            displayName: user.displayName,
+            email: user.email,
+          }
+        });
+
+      }
+    });
+
+  }
+}
+
+export default NavBar
--- src/index.js
+++ src/index.js
@@ -9,40 +9,39 @@ import firebaseConfig from './config/firebase';
 import registerServiceWorker from './registerServiceWorker';
 
 firebase.initializeApp(firebaseConfig);
-const provider = new firebase.auth.FacebookAuthProvider();
-provider.setCustomParameters({
-  'display': 'popup'
-});
+// const provider = new firebase.auth.FacebookAuthProvider();
+// provider.setCustomParameters({
+//   'display': 'redirect'
+// });
 
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
+// firebase.auth().onAuthStateChanged(function(user) {
+//   if (user) {
 
-    // User is signed in.
+//     // User is signed in.
 
-  } else {
+//   } else {
 
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-      var token = result.credential.accessToken;
-      // The signed-in user info.
-      var user = result.user;
+//     firebase.auth().signInWithPopup(provider).then(function(result) {
+//       // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+//       var token = result.credential.accessToken;
+//       // The signed-in user info.
+//       var user = result.user;
 
-      console.log(result.user)
-    }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
+//       console.log('Signed in as', token, user)
 
-      console.error('error', errorCode, errorMessage, email, credential)
-    });
-  }
-});
+//     }).catch(function(error) {
+//       // Handle Errors here.
+//       var errorCode = error.code;
+//       var errorMessage = error.message;
+//       // The email of the user's account used.
+//       var email = error.email;
+//       // The firebase.auth.AuthCredential type that was used.
+//       var credential = error.credential;
 
-
+//       console.error('error', errorCode, errorMessage, email, credential)
+//     });
+//   }
+// });
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
