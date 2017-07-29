f40198942820d799cd23b0281d877c91b38401fa
y-okada <y-okada@y-okada-no-MacBook-Air.local>
2017-07-29 17:35:29 +0900

    local commit

--- src/components/ProjectIndex.js
+++ src/components/ProjectIndex.js
@@ -10,6 +10,7 @@ class ProjectIndex extends Component {
     let title = 'みんなで起業！';
     let abstract = '日本にイノベーションを起こすために、新しい事業を創発しましょう。';
     let valuation = '¥10,000,000';
+    const pid = 'aaaaaa';
 
     let list = [];
     let data = [
@@ -30,7 +31,9 @@ class ProjectIndex extends Component {
             <div>{abstract}</div>
           </div>
           <div className="valuation-area">
-            <div>button</div>
+            <button>
+              <Link to={`/projects/${pid}`}><img src="" alt=">" /></Link>
+            </button>
           </div>
         </div>
       );
@@ -38,7 +41,7 @@ class ProjectIndex extends Component {
 
     return (
       <div className="App">
-        <h1>プロジェクト一覧画面</h1>
+        <h3>プロジェクト一覧画面</h3>
         <button>
           <Link to="/new-project">新規プロジェクト追加</Link>
         </button>
