38c6c22c05ed0c509ed6abf02320685068ba5211
Jouko Karvonen <joukokar@gmail.com>
2017-07-29 17:38:33 +0900

    Move initialization elsewhere

--- src/store/project.js
+++ src/store/project.js
@@ -1,5 +1,6 @@
+import {action, autorun, observable} from "mobx";
+
 import user from './user'
-import {observable, autorun, action} from "mobx";
 
 export default class Project {
     @observable image_url = "";
@@ -8,25 +9,3 @@ export default class Project {
     @observable valuation = "";
     @observable members = [];
 }
-
-let project = new Project()
-project.image_url = "https://static.pepy.jp/wp-content/uploads/2015/01/06115944/540bca3b8a7377bf8aabd4c1e751a31c-e1483577178335.jpg";
-project.title = "ハタラコ！！";
-project.abstract = "働け！";
-project.valuation = 100;
-
-let user = new User()
-user.name = "林田敦"
-
-let member = {
-  role : "エンジニア",
-  stock_amount : 100,
-  stock_share : 50,
-  user : user
-}
-
-projects.members.push(
-  member
-)
-
-export default project.project
--- src/store/projects.js
+++ src/store/projects.js
@@ -1,6 +1,7 @@
 import {action, autorun, observable} from "mobx";
 
 import Project from './project'
+import User from './user'
 
 class Projects {
     @observable projects = [];
@@ -29,5 +30,27 @@ project.abstract = '日本にイノベーションを起こすために、新し
 project.valuation = 1000;
 
 projects.projects.push(anotherProject)
+
+
+
+const newProject = new Project()
+newProject.image_url = "https://static.pepy.jp/wp-content/uploads/2015/01/06115944/540bca3b8a7377bf8aabd4c1e751a31c-e1483577178335.jpg";
+newProject.title = "ハタラコ！！";
+newProject.abstract = "働け！";
+newProject.valuation = 100;
+
+const user = new User()
+user.name = "林田敦"
+
+let member = {
+  role : "エンジニア",
+  stock_amount : 100,
+  stock_share : 50,
+  user : user
+}
+
+newProject.members.push(member)
+
+projects.projects.push(newProject)
 
 export default projects.projects
